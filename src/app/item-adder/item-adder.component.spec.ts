0f23aa1d2c487d2e93171e8802d521765c8d5f0d
Erik Gilling <konkers@konkers.net>
2018-12-19 15:13:20 -0800

    Add item adder to location detail page.

---
+++ src/app/item-adder/item-adder.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemAdderComponent } from './item-adder.component';
+
+describe('ItemAdderComponent', () => {
+  let component: ItemAdderComponent;
+  let fixture: ComponentFixture<ItemAdderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ItemAdderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ItemAdderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
