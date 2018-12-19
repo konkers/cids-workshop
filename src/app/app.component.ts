db89de0954b99fbec1669cc7b6894d2b945f8a78
Erik Gilling <konkers@konkers.net>
2018-12-19 12:01:44 -0800

    Add nav bar and action menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, Observable } from "rxjs";
 
 import { StateService } from "./state.service";
+import { Locations, LocationState, LocationService } from "./location.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "tracker";
 
   private _keyItemEvents: BehaviorSubject<string>;
@@ -21,7 +22,19 @@ export class AppComponent {
   private _bossEvents: BehaviorSubject<string>;
   bossEvents: Observable<string>;
 
-  constructor(private router: Router, private stateService: StateService) {
+  navTitle = "";
+  locs: Locations;
+  locRouteRe = RegExp("/loc/([^/]*)");
+  pageNames = {
+    "/items": "Items",
+    "/flags": "Flags"
+  };
+
+  constructor(
+    private router: Router,
+    private locationService: LocationService,
+    private stateService: StateService
+  ) {
     this._keyItemEvents = <BehaviorSubject<string>>(
       new BehaviorSubject(undefined)
     );
@@ -34,6 +47,33 @@ export class AppComponent {
 
     this._bossEvents = <BehaviorSubject<string>>new BehaviorSubject(undefined);
     this.bossEvents = this._bossEvents.asObservable();
+  }
+  ngOnInit() {
+    this.router.events.subscribe(res => {
+      this.processRoute();
+    });
+    this.locationService.getLocations().subscribe(locs => {
+      this.locs = locs;
+      this.processRoute();
+    });
+  }
+
+  private processLocationRoute(locId: string) {
+    if (this.locs && locId in this.locs) {
+      this.navTitle = this.locs[locId].name;
+    }
+  }
+  private processRoute() {
+    const url = this.router.url;
+    const match = this.locRouteRe.exec(url);
+    if (match) {
+      this.processLocationRoute(match[1]);
+      return;
+    }
+
+    if (url in this.pageNames) {
+      this.navTitle = this.pageNames[url];
+    }
   }
 
   goFlags() {
